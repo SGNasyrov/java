36a19795c39dbb3f7acee4ae6f6e890481f162e9
SGNasyrov <nasyrov.stas@gmail.com>
2021-03-19 10:11:38 +0300

    1

---
+++ Orange.java
@@ -0,0 +1,12 @@
+public class Orange extends Fruit {
+    private float weight = 1.5f;
+    private String name = "Orange";
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
